f99c8546361aea13a08e9d1228971b927e5df627
Reynold-Leido-2A <reynoldleido23@gmail.com>
2026-04-27 17:01:57 +0800

    implemet login functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use Log;
-
+//TODO : Implement login functionality
 class AuthController extends Controller
 {
     public function login(Request $request)
